236ae07551944276c22f1e4c278a66f6f9e64ca2
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-02-12 09:58:57 +0200

    finished app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setInputText(newValue);
+  }
+  
+  function addItem(e) {
+    e.preventDefault();
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
+      
       <div className="form">
-        <input type="text" />
-        <button>
+      <form>
+        <input type="text" value={inputText} onChange={handleChange}/>
+        <button type="submit" onClick={addItem}>
           <span>Add</span>
         </button>
+        </form>
+
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(item => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
+
     </div>
+    
   );
 }
 
